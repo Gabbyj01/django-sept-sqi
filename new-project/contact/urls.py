f988782b961479d13da5c6a3ddebf8ae13d17bc4
Gafar <gakinoso@yahoo.com>
2024-09-10 02:20:21 -0700

    add files to gitignore. do assignment to push project to git.

--- new-project/contact/urls.py
+++ new-project/contact/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from contact import views
 
 urlpatterns = [
-    path('contact-us/', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('email/', views.email, name='email'),
 ]
